8d4f4cc5a049afb3f6b8343230b8520444c44838
Stijn de Witt <StijnDeWitt@gmail.com>
2014-02-21 00:22:06 +0100

    Finishing touches.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 fb-colorize
 ===========
+
+Tool demonstrating the use of SVG filters in modern web browsers.
+
+Copyright 2014 by [Stijn de Witt](http://stijndewitt.com/), some rights reserved.
+Licensed under the Creative Commons Attribution 3.0 [license](https://creativecommons.org/licenses/by/3.0/).
--- fb-colorize.js
+++ fb-colorize.js
@@ -1,71 +1,83 @@
 ﻿(function fb_colorize(){
-	if (window['console']) console.info('fb-colorize: Starting...');
-	function byId(id) {return document.getElementById(id);}
-	var formPreview = byId('preview');
-	formPreview.addEventListener('submit', preview, false);
-	var formColorize = byId('colorize');
-	formColorize.addEventListener('submit', apply, false);
-	$('.preset').on('click', function() {
-		if (window['console']) console.info('fb-colorize: Applying preset...');
-		var radix = $(this).find('i')[0].innerText;
-		colorize(radix);
-	});
-	$('#input-fbc-slider').slider().on('slide', slide);
+	if (window['console']) console.log('fb-colorize: Starting...');
+	
+	if (window['console']) console.log('fb-colorize: Activating colorize slider...');
+	$('#input-fbc-slider').slider().on('slide', function slide(evt) {colorize(this.value);});
+	if (window['console']) console.log('fb-colorize: Activated colorize slider.');
+	
+	if (window['console']) console.log('fb-colorize: Activating colorize presets...');
+	$('.preset').on('click', function preset() {colorize($(this).find('i')[0].innerHTML);});
+	if (window['console']) console.log('fb-colorize: Activated colorize presets.');
+	
+	if (window['console']) console.log('fb-colorize: Activating colorize filter...');
+	$('#colorize').on('submit', function filter(evt) {colorize(); return false;});
+	if (window['console']) console.log('fb-colorize: Activated colorize filter.');
+	
+	if (window['console']) console.log('fb-colorize: Activating preview editor...');
+	$('#preview').on('submit', preview);
+	if (window['console']) console.log('fb-colorize: Activated preview editor.');
+	
 	if (window['console']) console.info('fb-colorize: Started.');
 	
+	function colorize(radix) {
+		if (radix) {
+			var code = $('#code-fbc-filter')[0];
+			code.value = '<svg height="0" width="0">\n' +
+					'  <filter id="fb-filter">\n' +
+					'    <feColorMatrix type="hueRotate" values="' + radix + '"/>\n' +
+					'  </filter>\n' +
+					'</svg>';
+			$('#input-fbc-slider').slider('setValue', radix);
+			$('#input-fbc-slider')[0].value = radix;
+		}
+		update('#code-fbc-filter', '#container-fbc-filter', 'HTML');
+		update('#code-fbc-filter', '#paste-fbc-filter', 'TEXT');
+	}
+	
 	function preview(evt){
-		if (window['console']) console.info('fb-colorize: Updating.');
+		if (window['console']) console.info('fb-colorize: Updating preview button...');
 		if (window['FB']) {
 			delete window.FB;
 			var old = document.getElementById('facebook-jssdk');
 			old.parentNode.removeChild(old);
 		}
-		update('code-fb-button', 'container-fb-button', 'HTML');
-		update('code-fb-sdk', 'container-fb-sdk', 'HTML');
+		update('#code-fb-button', '#container-fb-button', 'HTML');
+		update('#code-fb-sdk', '#container-fb-sdk', 'HTML');
 		evt.preventDefault();
 	}
 	
-	function slide(evt) {
-		if (window['console']) console.info('fb-colorize: Sliding...');
-		var slider = byId('input-fbc-slider');
-		colorize(slider.value);
-	}
-	
-	function apply(evt) {
-		if (evt && window['console']) console.info('fb-colorize: Applying colorize filter.');
-		colorize();
-		if (evt) evt.preventDefault();
-	}
-	
-	function colorize(radix) {
-		if (radix) {
-			var code = byId('code-fbc-filter');
-			code.value = '<svg height="0" width="0">\n' +
-					'\t<filter id="fb-filter">\n' +
-					'\t\t<feColorMatrix type="hueRotate" values="' + radix + '"/>\n' +
-					'\t</filter>\n' +
-					'</svg>';
-		}
-		update('code-fbc-filter', 'container-fbc-filter', 'HTML');
-		update('code-fbc-filter', 'paste-fbc-filter', 'Text');
-	}
-	
 	function update(src, dst, type) {
-		if (window['console']) console.info('updating ' + dst + '...');
-		var source = byId(src);
-		var dest = byId(dst);
+		if (window['console']) console.log('fb-colorize: Updating ' + dst + '...');
+		var source = $(src)[0];
+		var dest = $(dst)[0];
 		if (dest) {
-			var idx, data = source.value || source['inner' + type], text = dest['inner' + type];
+			var idx, text, data = get(source, "TEXT");
+			if (type == "TEXT") text = get(dest, "TEXT");
 			if (text && ((idx = text.indexOf('<style')) != -1))
 				data = data + '\n' + text.substring(idx);
-			dest['inner' + type] = data;
-			if (type == 'HTML') {
-				for (var i=0, elem; elem=dest.children[i]; i++) {
-					if (elem.tagName && elem.tagName.toLowerCase() == 'script')
-						execScript(elem);
-				}
+			
+			set(dest, type, data);
+
+		}
+	}
+	
+	function set(elem, type, value) {
+		elem[acc(elem, type)] = value;
+		if (type == "HTML") {
+			for (var i=0, e; e=elem.children[i]; i++) {
+				if (e.tagName && e.tagName.toLowerCase() == 'script')
+					execScript(e);
 			}
 		}
+	}
+	
+	function get(elem, type) {
+		return elem[acc(elem, type)];
+	}
+	
+	function acc(elem, type) {
+		return elem.nodeName.toLowerCase() == 'pre' ? 
+			"textContent" : (type == 'HTML' ? "innerHTML" : "value");
 	}
 	
 	function execScript(elem) {
